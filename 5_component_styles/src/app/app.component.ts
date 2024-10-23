b1c17676a5e768cc78873a6ffff80aa303e88741
Serge Gusarov <mrhersergik@gmail.com>
2024-10-23 21:17:15 +0700

    init

---
+++ 5_component_styles/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+ 
+@Component({
+    selector: "my-app",
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
